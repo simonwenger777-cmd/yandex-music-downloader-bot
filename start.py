b4b3e2bbf8c77b3d78d21dc9cd3634a15a834911
skriptojtelezka-eng <skriptojtelezka@gmail.com>
2025-12-27 00:26:45 +0400

    Fix: hardcode port to 10000 for Render

--- start.py
+++ start.py
@@ -3,5 +3,4 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
